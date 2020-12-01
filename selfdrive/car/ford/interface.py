4bb7c3aaca3aeb84de809b1c7084946b0caa977f
RoxasTheNobody98 <ebickels440@gmail.com>
2020-11-30 19:28:43 -0600

    FATAL: Remove gray panda remnants.

--- selfdrive/car/ford/interface.py
+++ selfdrive/car/ford/interface.py
@@ -3,8 +3,8 @@ from cereal import car
 from selfdrive.swaglog import cloudlog
 from selfdrive.config import Conversions as CV
 #from selfdrive.controls.lib.drive_helpers import EventTypes as ET, create_event
-from selfdrive.car.ford.values import MAX_ANGLE, Ecu, ECU_FINGERPRINT, FINGERPRINTS, CAR
-from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, is_ecu_disconnected, gen_empty_fingerprint
+from selfdrive.car.ford.values import MAX_ANGLE
+from selfdrive.car import STD_CARGO_KG, scale_rot_inertia, scale_tire_stiffness, gen_empty_fingerprint
 from selfdrive.car.interfaces import CarInterfaceBase
 
 
@@ -15,8 +15,8 @@ class CarInterface(CarInterfaceBase):
     return float(accel) / 3.0
 
   @staticmethod
-  def get_params(candidate, fingerprint=gen_empty_fingerprint(), has_relay=False, car_fw=[]): # pylint: disable=dangerous-default-value
-    ret = CarInterfaceBase.get_std_params(candidate, fingerprint, has_relay)
+  def get_params(candidate, fingerprint=gen_empty_fingerprint(), car_fw=[]): # pylint: disable=dangerous-default-value
+    ret = CarInterfaceBase.get_std_params(candidate, fingerprint)
     ret.carName = "ford"
     ret.communityFeature = True                              
     ret.safetyModel = car.CarParams.SafetyModel.ford
@@ -83,7 +83,7 @@ class CarInterface(CarInterfaceBase):
 
     ret.steerControlType = car.CarParams.SteerControlType.angle
 
-    ret.enableCamera = is_ecu_disconnected(fingerprint[0], FINGERPRINTS, ECU_FINGERPRINT, candidate, Ecu.fwdCamera) or has_relay
+    ret.enableCamera = True
     cloudlog.warning("ECU Camera Simulated: %r", ret.enableCamera)
 
     return ret
